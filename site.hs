2a87720845ed778628464fee294e019ab45cf7fc
Jim McStanton <jim@jhmcstanton.com>
2018-07-17 00:53:01 -0500

    Tweaked site to include subdirs for projects, posts, and brews. Tweaked css, and templates. Need to update posts/remove latin,etc

--- site.hs
+++ site.hs
@@ -21,32 +21,22 @@ main = hakyll $ do
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= relativizeUrls
 
-    match "posts/*" $ do
+    match "posts/**/*" $ do
         route $ setExtension "html"
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/post.html"    postCtx
             >>= loadAndApplyTemplate "templates/default.html" postCtx
             >>= relativizeUrls
 
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
-
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
-
+    createArchiveLanding "posts/blog/index.html" "Posts" "posts/blog/*.markdown"
+    createArchiveLanding "posts/brews/index.html" "Brews" "posts/brews/*.markdown"
+    createArchiveLanding "posts/projects/index.html" "Projects" "posts/projects/*.markdown"
+    createArchiveLanding "archive.html" "Archives" "posts/**/*.markdown"
 
     match "index.html" $ do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
+            posts <- recentFirst =<< loadAll "posts/**/*.markdown"
             let indexCtx =
                     listField "posts" postCtx (return posts) `mappend`
                     constField "title" "Home"                `mappend`
@@ -57,7 +47,7 @@ main = hakyll $ do
                 >>= loadAndApplyTemplate "templates/default.html" indexCtx
                 >>= relativizeUrls
 
-    match "templates/*" $ compile templateBodyCompiler
+    match "templates/**" $ compile templateBodyCompiler
 
 
 --------------------------------------------------------------------------------
@@ -65,3 +55,19 @@ postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
     defaultContext
+
+createArchiveLanding :: String -> String -> Pattern -> Rules ()
+createArchiveLanding page title glob =
+  create [fromFilePath page] $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll glob
+            let archiveCtx =
+                    listField "posts" postCtx (return posts) `mappend`
+                    constField "title" title            `mappend`
+                    defaultContext
+
+            makeItem ""
+                >>= loadAndApplyTemplate (fromFilePath $ "templates/" <> page) archiveCtx
+                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+                >>= relativizeUrls
