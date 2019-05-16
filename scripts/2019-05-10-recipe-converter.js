b41acc06b6c169e208572b712bff4f3c3d8c0a2e
jhmcstanton <jim@jhmcstanton.com>
2019-05-16 18:08:23 -0500

    Started poking at extract to all grain calc

---
+++ scripts/2019-05-10-recipe-converter.js
@@ -0,0 +1,15 @@
+const parentTable = Document.getElementsByTagName('table')[0];
+const dmeField = 4;
+const lmeField = dme_field + 1;
+
+const dmeTable = Document.getElementsByTagName('table')[1];
+const lmeTable = Document.getElementsByTagName('table')[2];
+
+const efficiency = a => {
+    alert(a);
+};
+alert(efficiency());
+
+// const setSG = (field, percent) => {
+    // parentTable.get
+// };
