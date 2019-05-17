ad6423c5ab6b1a2ab6c6f29ce44d3a752ca19502
jhmcstanton <jim@jhmcstanton.com>
2019-05-17 00:02:32 -0500

    Working for the most part. Probably broken in some places. Pushing anyway

--- scripts/2019-05-10-recipe-converter.js
+++ scripts/2019-05-10-recipe-converter.js
@@ -1,15 +1,66 @@
-const parentTable = Document.getElementsByTagName('table')[0];
-const dmeField = 4;
-const lmeField = dme_field + 1;
+const parentTable = document.getElementsByTagName('table')[0];
+const dmeField = 0;
+const lmeField = dmeField + 1;
 
-const dmeTable = Document.getElementsByTagName('table')[1];
-const lmeTable = Document.getElementsByTagName('table')[2];
+const dmeTable = document.getElementsByTagName('table')[1];
+const lmeTable = document.getElementsByTagName('table')[2];
 
-const efficiency = a => {
-    alert(a);
+const numField = function(id) {
+    return parseInt(document.getElementById(id).value);
+}
+const efficiency = function() {
+    return numField('efficiency');
 };
-alert(efficiency());
 
-// const setSG = (field, percent) => {
-    // parentTable.get
-// };
+const batchSize = function() {
+    return numField('batchSize');
+};
+
+const extractWeight = function(field) {
+    const weightNode = parentTable.getElementsByTagName('input')[field];
+    if ( weightNode.value === '' ) {
+        return 0;
+    }
+    return parseFloat(weightNode.value);
+}
+
+const sg = function(field, percent) {
+    const weight = extractWeight(field);
+    const sg = weight * 45 * percent / batchSize();
+    /* Yikes, side effects in a getter. If anyone is reading this, please don't
+     * do this and don't judge me :(
+     * OTOH, while we're in here lets just display the calculated sg
+     */
+	const weightNode = parentTable.getElementsByTagName('input')[field];
+    weightNode.parentNode.nextSibling.nextSibling.innerHTML = (sg / 1000) + 1;
+    return sg;	
+};
+
+const setGrainAmounts = function(table, extractWeight, sg) {
+    const rows = Array.from(table.getElementsByTagName('tr')).slice(1);
+	console.log('confused');
+	rows.forEach(function(row) {
+			setGrainAmount(row, extractWeight, sg);
+	});
+}
+
+const setGrainAmount = function(row, extractWeight, sg) {
+	const childValStr = row.childNodes[3].childNodes[0].value;
+	if (childValStr === '') {
+			return;
+	}
+	const childVal = parseInt(childValStr);
+	const grainWeight = childVal * extractWeight * sg / (sg * efficiency());
+	row.childNodes[5].innerHTML = grainWeight;
+}
+
+const updateAll = function() {
+    const dmeSg = sg(dmeField, 1);
+    const lmeSg = sg(lmeField, 0.8);
+	setGrainAmounts(dmeTable, extractWeight(dmeField), dmeSg);
+	setGrainAmounts(lmeTable, extractWeight(lmeField), lmeSg);
+}
+
+Array.from(document.getElementsByTagName('input')).forEach(function(input) {
+    input.addEventListener('input', updateAll);
+})
