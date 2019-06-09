28b40f9d2607fdf32983f271620390717333c152
Jim McStanton <jim@jhmcstanton.com>
2019-06-08 22:20:39 -0500

    Added clock script + partial

---
+++ scripts/countdown.js
@@ -0,0 +1,14 @@
+const tz = "America/Chicago";
+const clock_base = moment.tz(document.getElementById("clock_base").innerHTML, tz);
+const sections = document.getElementsByClassName('clock-section-amount');
+
+const update_clock = function() {
+    const now = moment.tz(tz);
+    sections[0].innerHTML = Math.abs(clock_base.diff(now, 'months'));
+    sections[1].innerHTML = Math.abs(clock_base.diff(now, 'days') % moment(now).daysInMonth());
+    sections[2].innerHTML = Math.abs(clock_base.diff(now, 'hours') % 24 );
+    sections[3].innerHTML = Math.abs(clock_base.diff(now, 'minutes') % 60);
+};
+update_clock();
+setInterval(update_clock, 1000 * 60);
+
