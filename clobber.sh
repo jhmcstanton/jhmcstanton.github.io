c20eb0a61744d5239cc864132ecd1d9673804175
Jim McStanton <jim@jhmcstanton.com>
2020-10-23 22:11:45 -0500

    Updated clobber to ignore modified files

--- clobber.sh
+++ clobber.sh
@@ -1,6 +1,6 @@
 #!/usr/local/bin/fish
 
 git checkout -- .
-for f in (git status | grep 'posts/' | grep -v "new file" | tr -d '\t')
+for f in (git status | grep 'posts/' | grep -v "new file: " | grep -v "modified: "tr -d '\t')
     rm $f
 end
