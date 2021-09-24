cadd24bfa81c322768b63577958ecc783f1f5c2f
Jim McStanton <jim@jhmcstanton.com>
2021-09-24 10:38:55 -0500

    Publish.

--- scripts/2020-10-25-key-signature-practice.js
+++ scripts/2020-10-25-key-signature-practice.js
@@ -1,59 +1,7 @@
-let key    = "C";
-const keys = ["Ab", "A", "Bb", "B", "Cb", "C", "C#", "Db", "D", "Eb", "E", "F", "F#", "Gb", "G"];
-const answerEl   = document.getElementById('answer');
-const resultEl   = document.getElementById('result');
-const clefEl     = document.getElementById('clef');
-const keyTypeEl  = document.getElementById('keytype');
-
-function checkAnswer() {
-    const answer = answerEl.value;
-    answer[0] = answer[0].toUpperCase();
-    return answer === key;
-}
-
-function wrongAnswerIndicator() {
-    answerEl.style.borderColor = "red";
-    resultEl.innerHTML         = "Incorrect";
-    resultEl.style.color       = "red";
-}
-
-function resetAnswerBox() {
-    answerEl.style.borderColor = "";
-}
-
-function quiz() {
-    if (checkAnswer()) {
-        resultEl.innerHTML   = "Correct!";
-        resultEl.style.color = "";
-        update();
-    } else {
-        wrongAnswerIndicator();
-    }
-}
-
-function newKey() {
-    let newKey = key;
-    while (newKey === key) {
-        newKey = keys[Math.floor(Math.random() * keys.length)];
-    }
-    key = newKey;
-    renderKey(key, keyTypeEl.value, clefEl.value, false);
-}
-
-function renderKey(key, ktype, clef, consolelog) {
-    const abc  = `X: 1
-L: 1/1
-K: ${key}${ktype} ${clef}
-|]`;
-    if (consolelog) {
-        console.log(abc);
-    }
-    ABCJS.renderAbc("paper", abc);
-}
-
-function update() {
-    resetAnswerBox();
-    newKey();
-}
-
-update();
+var key="C",keys=["Ab","A","Bb","B","Cb","C","C#","Db","D","Eb","E","F","F#","Gb","G"],answerEl=document.getElementById('answer'),resultEl=document.getElementById('result'),clefEl=document.getElementById('clef'),keyTypeEl=document.getElementById('keytype');function checkAnswer(){var answer=answerEl.value;answer[0]=answer[0].toUpperCase();return answer===key}
+function wrongAnswerIndicator(){answerEl.style.borderColor="red";resultEl.innerHTML="Incorrect";resultEl.style.color="red"}
+function resetAnswerBox(){answerEl.style.borderColor=""}
+function quiz(){if(checkAnswer()){resultEl.innerHTML="Correct!";resultEl.style.color="";update()}else wrongAnswerIndicator()}
+function newKey(){var newKey=key;while(newKey===key)newKey=keys[Math.floor(Math.random()*keys.length)];key=newKey;renderKey(key,keyTypeEl.value,clefEl.value,false)}
+function renderKey(key,ktype,clef,consolelog){var abc="X: 1\nL: 1/1\nK: "+key+ktype+" "+clef+"\n|]";if(consolelog)console.log(abc);ABCJS.renderAbc("paper",abc)}
+function update(){resetAnswerBox();newKey()};update()
--- scripts/countdown.js
+++ scripts/countdown.js
@@ -1,15 +1,1 @@
-const tz = "America/Chicago";
-const clock_base = moment.tz(document.getElementById("clock_base").innerHTML, tz);
-const sections = document.getElementsByClassName('clock-section-amount');
-
-const update_clock = function() {
-    const now = moment.tz(tz);
-    sections[0].innerHTML = Math.abs(clock_base.diff(now, 'months'));
-    sections[1].innerHTML = Math.abs(clock_base.diff(now, 'days') % moment(now).daysInMonth());
-    sections[2].innerHTML = Math.abs(clock_base.diff(now, 'hours') % 24 );
-    sections[3].innerHTML = Math.abs(clock_base.diff(now, 'minutes') % 60);
-    sections[4].innerHTML = Math.abs(clock_base.diff(now, 'seconds') % 60);
-};
-update_clock();
-setInterval(update_clock, 1000);
-
+var tz="America/Chicago",clock_base=moment.tz(document.getElementById("clock_base").innerHTML,tz),sections=document.getElementsByClassName('clock-section-amount'),update_clock=function(){var now=moment.tz(tz);sections[0].innerHTML=Math.abs(clock_base.diff(now,'months'));sections[1].innerHTML=Math.abs(clock_base.diff(now,'days')%moment(now).daysInMonth());sections[2].innerHTML=Math.abs(clock_base.diff(now,'hours')%24);sections[3].innerHTML=Math.abs(clock_base.diff(now,'minutes')%60);sections[4].innerHTML=Math.abs(clock_base.diff(now,'seconds')%60)};update_clock();setInterval(update_clock,1000)
